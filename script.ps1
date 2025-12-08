0ea4bea9c6679c8cfacc93c1c02ff0a269dcb032
keylaim100 <keimbaun@uide.edu.ec>
2025-12-08 12:40:32 +0000

    primer commit

---
+++ script.ps1
@@ -0,0 +1,25 @@
+function Start-ProgressBar {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Title,
+
+        [Parameter(Mandatory = $true)]
+        [int]$Timer
+    )
+
+    for ($i = 1; $i -le $Timer; $i++) {
+        Start-Sleep -Seconds 1
+
+        $percent = [int](($i / $Timer) * 100)
+
+        Write-Progress `
+            -Activity $Title `
+            -Status "Segundo $i de $Timer" `
+            -PercentComplete $percent
+    }
+
+    Write-Progress -Activity $Title -Status "Completado" -Completed
+}
+
+Start-ProgressBar -Title "Test timeout" -Timer 30
