707f352bcb5fb472cfc7f6d2d8083b82120faf77
keylaim100 <keimbaun@uide.edu.ec>
2025-12-08 13:26:10 +0000

    segundo commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # powershell_scripts
+![alt text](image.png)
+
--- script.ps1
+++ script.ps1
@@ -1,25 +1,21 @@
-function Start-ProgressBar {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $true)]
-        [string]$Title,
-
-        [Parameter(Mandatory = $true)]
-        [int]$Timer
+function Start-ProgressBar  # Esta línea crea una función nueva llamada Start-ProgressBar
+{
+    [CmdletBinding()] # Esta etiqueta vuelve a la función una función avanzada que permite hacer mas cosas que un cmdlet normal
+    param # Aquí se definen los parámetros que recibe la función
+    (
+        [Parameter(Mandatory = $true)] # Este bloque dice que $Title es un parámetro obligatorio
+        $Title, # $Title será un texto que se usa como título de la barra de progreso
+        [Parameter(Mandatory = $true)] # Igual que el anterior pero para $Timer, también es obligatorio y además se le pone [int] para indicar que debe ser un número entero
+        [int]$Timer                    # representa cuántos segundos durará el conteo de la barra de progreso
     )
 
-    for ($i = 1; $i -le $Timer; $i++) {
-        Start-Sleep -Seconds 1
-
-        $percent = [int](($i / $Timer) * 100)
-
-        Write-Progress `
-            -Activity $Title `
-            -Status "Segundo $i de $Timer" `
-            -PercentComplete $percent
+    For ($i = 1; $i -le $Timer; $i++) # Este es un bucle que se repite varias veces empieza con $i = 1 y se va incrementando de 1 en 1 ($i++) mientras $i sea menor o igual que $Timer
+    {                                 # En cada vuelta del bucle se simula que ha pasado un segundo y se actualiza la barra de progreso
+        Start-Sleep -Seconds 1 # Sirve para que la barra no se llene rapido sino que avance poco a poco cada segundo como un temporizador
+        $percentComplete = ($i / $Timer)*100 # Aquí se calcula el porcentaje de avance se divide el segundo actual para el total de segundos para obtener la fraccion de progreso y se multiplica por 100 para convertirla en porcentaje
+        Write-Progress -Activity $Title -Status "$i second elapsed" -PercentComplete $percentComplete # Esta linea dibuja la barra de progreso en la consola -Activity muestra el título que se paso en $Title, -Status muestra un texto que indica cuántos segundos han pasado
+                                                                                                      # y -PercentComplete usa el porcentaje calculado para mostrar cuanto de la barra debe estar llena 
     }
+} #Function Start-ProgressBar
 
-    Write-Progress -Activity $Title -Status "Completado" -Completed
-}
-
-Start-ProgressBar -Title "Test timeout" -Timer 30
+Start-ProgressBar -Title "Test timeout" -Timer 30 # Esta linea es la llamada final a la función
